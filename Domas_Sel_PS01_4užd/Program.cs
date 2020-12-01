b61c0b190b0eac07889fa0e98e3ab551b0a3fc94
Daimyo <domas.sel640@go.kauko.lt>
2020-12-01 11:03:39 +0200

    a

---
+++ Domas_Sel_PS01_4užd/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Domas_Sel_PS01_4užd
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
