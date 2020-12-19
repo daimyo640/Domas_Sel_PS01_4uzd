aeef649d4e94421f351530071d13e170582c6566
Daimyo <domas.sel640@go.kauko.lt>
2020-12-19 02:17:57 +0200

    Atlikta užduotis.

--- Domas_Sel_PS01_4užd/Form1.cs
+++ Domas_Sel_PS01_4užd/Form1.cs
@@ -12,9 +12,49 @@ namespace Domas_Sel_PS01_4užd
 {
     public partial class Form1 : Form
     {
+        public double[] sarasas = new double[10];
+        public int sk = 0;
+        public double suma=0.0;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (sk <= 9)
+            {
+                String ivestis = textBox1.Text;
+                double.TryParse(ivestis, out double a);
+                sarasas[sk] = a;             
+                label2.Text = "Įveskite dar " + (10-sk).ToString() + " skaičius.";
+                sk++;
+            }
+            
+            else
+            {
+                for(int i=0; i<=9; i++)
+                {
+                    suma += sarasas[i];
+                    label2.Text = suma.ToString();
+                }
+                button1.Enabled = false;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String ivestis = textBox2.Text;
+            int.TryParse(ivestis, out int a);
+            int tarpu_sk = a;
+
+            for(int i=0; i<a; i++)
+            {               
+                richTextBox1.AppendText(String.Concat(Enumerable.Repeat(" ", tarpu_sk)));
+                tarpu_sk--;
+                richTextBox1.AppendText(String.Concat(Enumerable.Repeat("* ", i+1)));
+                richTextBox1.AppendText("\r\n");
+            }
         }
     }
 }
